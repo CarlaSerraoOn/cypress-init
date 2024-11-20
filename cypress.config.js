fe57cceda4ff31857ee61dd7e12ad7b0799639d5
CarlaSerraoOn <carla.suporteam@gmail.com>
2024-11-20 16:56:51 -0400

    projeto finalizado

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  
   e2e: {
     baseUrl:'https://automationpratice.com.br/',
     defaultCommandTimeout: 5000,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
     screenshotOnRunFailure: true, // Captura screenshots em caso de falha
     video: true, // Registra vídeos dos testes
   },
--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -6,51 +6,63 @@ import register_page from '../support/pages/register_page';
 
 const user_data = require('../fixtures/data_valid.json')
 
-describe('Cadastro de usuário', () => {
+// const screens = ['desktop', 'iphone-x', 'iphone-8']
+
+// screens.forEach(element => {
+
+  describe('Cadastro de usuário', () => {
 
     beforeEach(() => {
-       home_page.accessRegisterPage()
-      });
-      
-      it.only('Validar campo nome vazio', () => {
-        register_page.saveRegister()
-        register_page.checkMessage('O campo nome deve ser prenchido')
-      })
 
-      it.only('Validar campo email vazio', () => {
-        register_page.fillName(user_data.name)
-        register_page.saveRegister()
-        register_page.checkMessage('O campo e-mail deve ser prenchido corretamente')
-      })
+      // if(element != 'desktop') {
+      //   cy.viewport(element)
+      // }
 
-      it.only('Validar campo email inválido', () => {
-        register_page.fillName(user_data.name)
-        register_page.fillEmail('emailinvalido')
-        register_page.saveRegister()
-        register_page.checkMessage('O campo e-mail deve ser prenchido corretamente')
-      })
 
-      it.only('Campo Senha invalido ', () => {
-        register_page.fillName(user_data.name)
-        register_page.fillEmail(user_data.email)
-        register_page.fillPassword('A123@')
-        register_page.saveRegister()
-        register_page.checkMessage('O campo senha deve ter pelo menos 6 dígitos')
-      })
-
-      it.only('Login com sucesso', () => {
-        const name = faker.person.fullName()
-        const email = faker.internet.email()
-
-        register_page.fillName(name)
-        register_page.fillEmail(email)
-        register_page.fillPassword(user_data.password)
-        register_page.saveRegister()
-        register_page.checkRegisterSucess(name)
-
+        home_page.accessRegisterPage()
+    });
         
+    it.only('Validar campo nome vazio', () => {
+      register_page.saveRegister()
+      register_page.checkMessage('O campo nome deve ser prenchido')
+    })
 
-      })
+    it.only('Validar campo email vazio', () => {
+      register_page.fillName(user_data.name)
+      register_page.saveRegister()
+      register_page.checkMessage('O campo e-mail deve ser prenchido corretamente')
+    })
+
+    it.only('Validar campo email inválido', () => {
+      register_page.fillName(user_data.name)
+      register_page.fillEmail('emailinvalido')
+      register_page.saveRegister()
+      register_page.checkMessage('O campo e-mail deve ser prenchido corretamente')
+    })
+
+    it.only('Campo Senha invalido ', () => {
+      register_page.fillName(user_data.name)
+      register_page.fillEmail(user_data.email)
+      register_page.fillPassword('A123@')
+      register_page.saveRegister()
+      register_page.checkMessage('O campo senha deve ter pelo menos 6 dígitos')
+    })
+
+    it.only('Login com sucesso', () => {
+      const name = faker.person.fullName()
+      const email = faker.internet.email()
+
+      register_page.fillName(name)
+      register_page.fillEmail(email)
+      register_page.fillPassword(user_data.password)
+      register_page.saveRegister()
+      register_page.checkRegisterSucess(name)
+
+          
+
+     })
 
 
-})
+  })
+
+// });
